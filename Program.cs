f41c262e983fe75010277b56b8ad2c29bfc2d555
Felipe Marini <felipemarini71188@gmail.com>
2020-11-23 18:27:36 -0300

    Algorithm still not finished

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Ex2_ListaFixacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double calculo, notafiscal;
+
+
+            Console.WriteLine();
+            
+            Console.WriteLine("Qual o tipo de combustível foi vendido? A(álcool) ou G(gasolina)? Por gentileza escrever A ou G apenas: \n");
+            string combustivel = Console.ReadLine().ToLower();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Quantos litros foram vendidos do combustível?: \n");
+            double litros = double.Parse(Console.ReadLine());
+
+            switch (combustivel) { 
+                
+                case "a": if (litros <= 20) {calculo = litros * (4.90 * 0.97);}
+                          
+                          else {calculo = litros * (4.90 * 0.95);};
+                          
+                          break;
+
+                case "g": if(litros <= 20) {calculo = litros * (5.30 * 0.96);}
+
+                          else {calculo = litros * (5.30 * 0.94);};
+
+                          break;
+                                 }
+
+                Console.WriteLine();
+
+                notafiscal = calculo;
+
+                Console.WriteLine($"O total a ser pago deverá ser: ${notafiscal}");
+
+
+
+
+
+
+        }
+    }
+}
